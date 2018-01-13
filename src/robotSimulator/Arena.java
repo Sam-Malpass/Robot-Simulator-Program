24965ececec2ccabb5c0cdbef12ca709fa7b4e83
Sam <sam@malpass.me.uk>
2018-01-13 23:25:45 +0000

    Add variables to Arena

--- src/robotSimulator/Arena.java
+++ src/robotSimulator/Arena.java
@@ -1,6 +1,32 @@
 package robotSimulator;
 
+import java.util.ArrayList;
+
+/**
+ * @author      Sam Malpass <gf009788@live.reading.ac.uk>
+ * @version     1.0
+ * @since       1.0        
+ */
 public class Arena 
 {
-
+	/**
+	 * serialVersionUID is used for saving created objects
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Width holds the current width of the object.
+	 */
+	private int Width;
+	/**
+	 * Length holds the current length of the object.
+	 */
+	private int Length;
+	/**
+	 * MaxCapacity holds the maximum capacity of the object.
+	 */
+	private int MaxCapacity;
+	/**
+	 * Contents holds the current contents of the object.
+	 */
+	protected ArrayList<ArenaObject> Contents;
 }
