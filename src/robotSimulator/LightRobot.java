d64435ee59af29a03c121f41ae45b47e2a255a97
Sam <sam@malpass.me.uk>
2018-01-14 09:32:30 +0000

    Create LightRobot class

---
+++ src/robotSimulator/LightRobot.java
@@ -0,0 +1,11 @@
+package robotSimulator;
+/**
+ * @author      Sam Malpass <gf009788@live.reading.ac.uk>
+ * @version     1.0
+ * @since       1.0        
+ */
+@SuppressWarnings("serial")
+public class LightRobot extends BasicRobot
+{
+
+}
