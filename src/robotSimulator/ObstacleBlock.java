8b77ada8265d1099ab28e0cc6197d89635fb92a4
Sam <sam@malpass.me.uk>
2018-01-14 09:15:38 +0000

    Add AttemptMove function to ObstacleBlock

--- src/robotSimulator/ObstacleBlock.java
+++ src/robotSimulator/ObstacleBlock.java
@@ -52,4 +52,16 @@ public class ObstacleBlock extends ArenaObject
 		/*Sets Arena to A*/
 		this.SetArena(A);
 	}
+	/**
+	 * Function definition for AttemptMove()
+	 * <p>
+	 * Handles moving the object by an X or a Y Position
+	 * <p>
+	 * @return instantly as the object doesn't move.
+	 */
+	public void AttemptMove()
+	{
+		/*Returns*/
+		return;
+	}
 }
