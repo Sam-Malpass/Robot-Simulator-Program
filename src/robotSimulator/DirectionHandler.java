7c69e53e76278afedb3ca4f9777a0c2e3e870186
Sam <sam@malpass.me.uk>
2018-01-14 08:56:33 +0000

    Add RandomDirection function to DirectionHandler

--- src/robotSimulator/DirectionHandler.java
+++ src/robotSimulator/DirectionHandler.java
@@ -1,4 +1,7 @@
 package robotSimulator;
+
+import java.util.Random;
+
 /**
  * @author      Sam Malpass <gf009788@live.reading.ac.uk>
  * @version     1.0
@@ -11,4 +14,20 @@ public enum DirectionHandler
 	 * a robot can be travelling in.
 	 */
 	UP, RIGHT, DOWN, LEFT;
+	/**
+	 * Function definition for RandomDirection()
+	 * <p>
+	 * Creates an object of type Random and then chooses a random number out of the
+	 * values in the enumerator. This random number corresponds to a direction which
+	 * is returned by the function.
+	 * <p>
+	 * @return a random direction
+	 */
+	public static DirectionHandler RandomDirection()
+	{
+		/*Creates object R of type Random*/
+		Random R = new Random();
+		/*Return a random direction*/
+		return values()[R.nextInt(values().length)];
+	}
 }
