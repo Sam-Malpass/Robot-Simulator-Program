7ac46ec59c75fd2c23bb434b7424c6ee5bd368ca
Sam <sam@malpass.me.uk>
2018-01-14 09:48:30 +0000

    Add Save function to FileIO

--- src/robotSimulator/FileIO.java
+++ src/robotSimulator/FileIO.java
@@ -1,4 +1,12 @@
 package robotSimulator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
 /**
  * @author      Sam Malpass <gf009788@live.reading.ac.uk>
  * @version     1.0
@@ -11,5 +19,46 @@ public class FileIO
 	 */
 	FileIO()
 	{	
+	}
+	/**
+	 * Function definition for Save()
+	 * <p>
+	 * Opens the JFrame file save dialog allowing users to name file
+	 * and amending .ser if the user has not done so already before finally
+	 * writing the Arena object to the file.
+	 *<p>
+	 * @param A is an Arena object that will contain various numbers of objects
+	 */
+	public void Save(Arena A) 
+	{
+		/*Object Declarations*/
+		JFrame Frame = new JFrame();
+		JFileChooser SaveAs = new JFileChooser();
+		/*Sets the Title of the Dialog Window to "Save As"*/
+		SaveAs.setDialogTitle("Save As");
+		/*Variable Declaration and Assignment*/
+		int Option = SaveAs.showSaveDialog(Frame);
+		/*If the Option is Approved*/
+		if (Option == JFileChooser.APPROVE_OPTION) 
+		{
+			/*Set File to be Equal to Selected File*/
+			File File = SaveAs.getSelectedFile();
+			/*Variable Declaration and Assignment*/
+			String FilePath = File.getPath();
+			/*Adds the ".ser" File Extension for the user*/
+			if(!FilePath.toLowerCase().endsWith(".ser"))	
+			{
+			    FilePath = FilePath + ".ser";
+			}
+			/*Attempts to Write to File*/
+			try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FilePath))) 
+			{
+				oos.writeObject(A);
+			} 
+			catch (Exception ex) 
+			{
+				ex.printStackTrace();
+			}
+		}	
 	}
 }
