0b67cfda6241455dfb7fad1c0e024851258efb0c
Sam <sam@malpass.me.uk>
2018-01-14 09:58:57 +0000

    Add variables to GUI

--- src/robotSimulator/GUI.java
+++ src/robotSimulator/GUI.java
@@ -1,6 +1,10 @@
 package robotSimulator;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 /**
  * @author      Sam Malpass <gf009788@live.reading.ac.uk>
  * @version     1.0
@@ -8,5 +12,32 @@ import javafx.application.Application;
  */
 public class GUI extends Application
 {
-
+	/**
+	 * CanvasSize is acts as the width and length of the canvas
+	 */
+	private int CanvasSize = 500;
+	/**
+	 * Arena is an object of type Arena
+	 */
+	private Arena Arena;
+	/**
+	 * gc is the GraphicsContext of the application
+	 */
+	private GraphicsContext gc;
+	/**
+	 * rtPane is a vertical box on the right hand side of the application
+	 */
+	private VBox rtPane;
+	/**
+	 * ltPane is a horizontal box going from left to right
+	 */
+	private HBox ltPane;
+	/**
+	 * Active is a boolean value that says whether an Arena object is open or not
+	 */
+	private boolean Active = false;
+	/**
+	 * Timer acts as a measure for the Animation
+	 */
+	private AnimationTimer Timer;
 }
