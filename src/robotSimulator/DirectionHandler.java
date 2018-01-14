0dea6d19c22aa429e7c40b04e47b13550da14ace
Sam <sam@malpass.me.uk>
2018-01-14 08:55:53 +0000

    Add the directions to DirectionHandler

--- src/robotSimulator/DirectionHandler.java
+++ src/robotSimulator/DirectionHandler.java
@@ -1,6 +1,14 @@
 package robotSimulator;
-
+/**
+ * @author      Sam Malpass <gf009788@live.reading.ac.uk>
+ * @version     1.0
+ * @since       1.0        
+ */
 public enum DirectionHandler 
 {
-
+	/**
+	 * UP, RIGHT, DOWN and LEFT are the four possible directions
+	 * a robot can be travelling in.
+	 */
+	UP, RIGHT, DOWN, LEFT;
 }
