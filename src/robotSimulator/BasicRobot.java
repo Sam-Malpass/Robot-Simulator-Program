4e12425451dcb0aded7bf06375e4d6ee1af2b72b
Sam <sam@malpass.me.uk>
2018-01-14 09:00:52 +0000

    Add constructors to BasicRobot

--- src/robotSimulator/BasicRobot.java
+++ src/robotSimulator/BasicRobot.java
@@ -14,4 +14,31 @@ public class BasicRobot extends ArenaObject
 	 * Direction holds the robot's current direction.
 	 */
 	private DirectionHandler Direction;
+	/**
+	 * Constructor with arguments.
+	 * <p>
+	 * Constructs a BasicRobot object using values passed to the 
+	 * constructor.
+	 * <p>
+	 * @param X is used to set the XPosition
+	 * @param Y is used to set the YPosition
+	 * @param A is used to set the Arena
+	 */
+	BasicRobot(int X, int Y, Arena A)
+	{
+		/*Sets ID*/
+		this.SetID(IDCalculation());
+		/*Sets XPosition to X*/
+		this.SetXPosition(X);
+		/*Sets YPosition to Y*/
+		this.SetYPosition(Y);
+		/*Sets Size*/
+		this.SetSize(20);
+		/*Sets Solid*/
+		this.SetSolid(true);
+		/*Sets Arena to A*/
+		this.SetArena(A);
+		/*Sets Direction to a random direction*/
+		this.SetDirection(DirectionHandler.RandomDirection());
+	}
 }
