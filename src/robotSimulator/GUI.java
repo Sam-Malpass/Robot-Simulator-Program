c7623c2767f8cf93bcf26f36eaa2c0fd06e2c59d
Sam <sam@malpass.me.uk>
2018-01-14 09:57:32 +0000

    Create GUI class

---
+++ src/robotSimulator/GUI.java
@@ -0,0 +1,12 @@
+package robotSimulator;
+
+import javafx.application.Application;
+/**
+ * @author      Sam Malpass <gf009788@live.reading.ac.uk>
+ * @version     1.0
+ * @since       1.0        
+ */
+public class GUI extends Application
+{
+
+}
